6f0d30267dfa81894f543a2e5c1ebbec2b14f573
Businge931 <jimmybusinge9@gmail.com>
2022-09-29 17:12:58 +0300

    removed unnecessary div

--- tipcalculator/src/components/Button/Button.jsx
+++ tipcalculator/src/components/Button/Button.jsx
@@ -3,15 +3,13 @@ import classes from "./Button.module.css";
 
 const Button = ({ children, onClick, type, isActive }) => {
   return (
-    <div>
-      <button
-        type={type}
-        onClick={onClick}
-        className={`${classes.button} ${isActive ? classes.active : ""}`}
-      >
-        {children}
-      </button>
-    </div>
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${classes.button} ${isActive ? classes.active : ""}`}
+    >
+      {children}
+    </button>
   );
 };
 
