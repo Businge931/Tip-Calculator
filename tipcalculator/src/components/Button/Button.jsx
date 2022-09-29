8701df6ad1181b95acf318642363bce9fee740f4
Businge931 <jimmybusinge9@gmail.com>
2022-09-29 10:14:43 +0300

    added active class

--- tipcalculator/src/components/Button/Button.jsx
+++ tipcalculator/src/components/Button/Button.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import classes from "./Button.module.css";
 
-const Button = ({ children, onClick, type }) => {
+const Button = ({ children, onClick, type, isActive }) => {
   return (
     <div>
-      <button type={type} onClick={onClick} className={classes.button}>
+      <button
+        type={type}
+        onClick={onClick}
+        className={`${classes.button} ${isActive ? classes.active : ""}`}
+      >
         {children}
       </button>
     </div>
